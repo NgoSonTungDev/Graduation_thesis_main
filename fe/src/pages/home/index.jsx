d172a05c6d4434851e6ae69ef3aa0493cae30e91
Ngo Tung <tungns@danateq.vn>
2023-02-14 09:55:04 +0700

    call api

--- fe/src/pages/home/index.jsx
+++ fe/src/pages/home/index.jsx
@@ -3,6 +3,13 @@ import { userApi } from "../../api/auther";
 
 const Home = () => {
   const fetchData = async () => {
+    // không cần check truong hop thành công
+
+    // const data = userApi.getUser();
+    // console.log(data);
+
+    // khi thành công thi làm việc tiêp theo
+
     await userApi.getUser().then((res) => {
       alert("thanh cong");
       console.log(res);
