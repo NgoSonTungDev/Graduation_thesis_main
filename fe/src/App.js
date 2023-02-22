d90c34acda7bd3f93b50bdd0a789458fe215549b
Ngo Tung <tungns@danateq.vn>
2023-02-22 16:31:36 +0700

    err menu

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,10 +1,9 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
-import Navbar from "./components/navbar";
-import Footer from "./components/footer";
 import Review from "./pages/review";
 import Register from "./pages/auth/register";
+import Navbar from "./components/navbar";
 const App = () => {
   return (
     <div>
