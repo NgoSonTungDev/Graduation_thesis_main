bf233e94427ee1fd518d692a174a9d96376f8bc4
Ngo Tung <tungns@danateq.vn>
2023-02-22 17:44:45 +0700

    fix toast

--- fe/src/App.js
+++ fe/src/App.js
@@ -4,6 +4,8 @@ import Home from "./pages/home";
 import Review from "./pages/review";
 import Register from "./pages/auth/register";
 import Navbar from "./components/navbar";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 const App = () => {
   return (
     <div>
@@ -16,6 +18,17 @@ const App = () => {
 
           {/* <Route path="*" element={<PageNotFound />} /> */}
         </Routes>
+        <ToastContainer
+          autoClose={1000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
+        <ToastContainer />
       </BrowserRouter>
     </div>
   );
