db9cc74c34c95505436f68ec1717b4eb9b4c1ac7
phantanphu1 <phantanphu1505@gmail.com>
2023-02-25 14:55:27 +0700

    design page review

--- fe/src/pages/review/index.jsx
+++ fe/src/pages/review/index.jsx
@@ -1,296 +1,224 @@
-import React, { useRef, useState } from "react";
-import "./index.scss"
-import Navbar from "../../components/navbar/index"
-import Rating from "@mui/material/Rating";
-import { Button, Input, message, Rate } from "antd";
-import { CloseOutlined, CameraOutlined, EnvironmentOutlined } from "@ant-design/icons";
+import React, { useRef, useState, useEffect } from "react";
+import "./index.scss";
+import Navbar from "../../components/navbar/index";
+import { Button, Input, Rate } from "antd";
+import axios from "axios";
+import {
+  CloseOutlined,
+  CameraOutlined,
+  EnvironmentOutlined,
+} from "@ant-design/icons";
 import PostModal from "./modal";
 const Review = () => {
-    const desc = ["Tệ", "Khá tệ", "Trung bình", "Tốt", "Tuyệt vời"];
-    const [content, setContent] = useState("");
-    const [images, setImages] = useState([]);
-    const [imageShow, setimageShow] = useState([]);
-    const [videoShow, setvideoShow] = useState([]);
-    const [placeSelected, setplaceSelected] = useState([]);
+  const desc = ["Tệ", "Khá tệ", "Trung bình", "Tốt", "Tuyệt vời"];
 
-    const fileRef = useRef([]);
+  const [content, setContent] = useState("");
 
-    const [rate, setRate] = useState({
-        rateDrink: 5,
-        ratePosition: 5,
-        ratePrice: 5,
-        rateService: 5,
-        rateView: 5,
-    });
+  const [file, setFile] = useState(null);
+  const [image2, setImage2] = useState("");
+  const [image, setImage] = useState("");
+  const [time, setTime] = useState("");
+  const [placeId, setPlaceId] = useState("");
 
-    const [open, setOpen] = useState(false);
+  const [data, setData] = useState([]);
+  const [open, setOpen] = useState(false);
+  const fileRef = useRef([]);
 
-    const handleOpen = () => {
-        setOpen(true)
+  const [rate, setRate] = useState({
+    rate: 4,
+  });
+
+  const addPost = (e) => {
+    axios
+      .post("http://localhost:4000/api/post/add", {
+        content: content,
+        image: image,
+        rating: rate,
+        time: time,
+        placeId: placeId,
+      })
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleSubmit = (e) => {
+    const formData = new FormData();
+    formData.append("photo", file);
+    axios
+      .post("http://localhost:4000/api/upload/file", formData, {
+        headers: { "Content-Type": "multipart/form-data" },
+      })
+      .then((res) => {
+        setImage2(res.data.path);
+        addPost(content, image, rate, time, placeId);
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleChangeFileImage = (e) => {
+    const file = e.target.files[0];
+    setImage(URL.createObjectURL(file));
+    setFile(file);
+  };
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleChange = (name, value) => {
+    setRate((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const deleteImages = (index) => {
+    const newArr = [...image];
+    newArr.splice(index, 1);
+
+    if (newArr.length === 0) {
+      if (fileRef.current) {
+        fileRef.current.value = null;
+      }
     }
 
-    const handleClose = () => {
-        setOpen(false)
-    }
-    const handleChange = (name, value) => {
-        setRate((prev) => ({
-            ...prev,
-            [name]: value,
-        }));
-    };
+    setImage(newArr);
+  };
+  const fetchData = (url) => {
+    axios
+      .get(url)
+      .then(function (response) {
+        setData(response.data.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
 
-    const handleChangeImages = (e) => {
-        const files = [...e.target.files];
-        let error = "";
-        let newImages = ([]);
-        files.forEach((file) => {
-            if (!file) {
-                error = "File không tồn tại.";
-                return;
-            }
-            if (file.size > 1024 * 1024 * 5) {
-                // 1mb
-                error = "File có dùng lượng quá 5mb.";
-                return;
-            }
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    let url = "http://localhost:4000/api/post/add";
+    fetchData(url);
+  }, []);
 
-            return newImages.push(file);
-        });
-
-        if (error) {
-            message.error(error);
-        }
-        setImages([...images, ...newImages]);
-    };
-
-    const deleteImages = (index) => {
-        const newArr = [...images];
-        newArr.splice(index, 1);
-
-        if (newArr.length === 0) {
-            if (fileRef.current) {
-                fileRef.current.value = null;
-            }
-        }
-
-        setImages(newArr);
-    };
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        if (images.length === 0 && !content) {
-            message.error("Vui lòng điền đầy đủ thông tin!");
-            return;
-        }
-
-        if (!placeSelected) {
-            message.error("Vui lòng chọn địa điểm để review");
-            return;
-        }
-    }
-    return (
-        <div>
-            <Navbar />
-            <div className="Review_Container" style={{ witdt: "50%" }}>
-                <div className="header">
-                    <h1>Viết Review</h1>
-                </div>
-            <PostModal open={open} />
-
-                <div className="Review">
-                    <div className="review-input">
-                        <h3>Xếp hạng của bạn</h3>
-                        <div className="rate-item">
-                            <div className="rate-item-cate">Vị trí</div>
-                            <Rate
-                                style={{ color: "red", fontSize: 32 }}
-                                onChange={(value) => handleChange("ratePosition", value)}
-                                value={rate.ratePosition}
-                            />
-                            {rate.ratePosition ? (
-                                <div className="rate-type">
-                                    <div className="rate-type-text">
-                                        <span>{desc[rate.ratePosition - 1]}</span>
-                                    </div>
-                                </div>
-                            ) : (
-                                <div className="rate-type">
-                                    <div className="rate-type-text">
-                                        <span>Rất tệ</span>
-                                    </div>
-                                </div>
-                            )}
-                        </div>
-                        <div className="rate-item">
-                            <div className="rate-item-cate">Không gian</div>
-                            <Rate
-                                style={{ color: "red", fontSize: 32 }}
-                                onChange={(value) => handleChange("rateView", value)}
-                                value={rate.rateView}
-                            />
-                            {rate.rateView ? (
-                                <div className="rate-type">
-                                    <div className="rate-type-text">
-                                        <span>{desc[rate.rateView - 1]}</span>
-                                    </div>
-                                </div>
-                            ) : (
-                                <div className="rate-type">
-                                    <div className="rate-type-text">
-                                        <span>Rất tệ</span>
-                                    </div>
-                                </div>
-                            )}
-                        </div>
-                        <div className="rate-item">
-                            <div className="rate-item-cate">Đồ uống</div>
-                            <Rate
-                                style={{ color: "red", fontSize: 32 }}
-                                onChange={(value) => handleChange("rateDrink", value)}
-                                value={rate.rateDrink}
-                            />
-                            {rate.rateDrink ? (
-                                <div className="rate-type">
-                                    <div className="rate-type-text">
-                                        <span>{desc[rate.rateDrink - 1]}</span>
-                                    </div>
-                                </div>
-                            ) : (
-                                <div className="rate-type">
-                                    <div className="rate-type-text">
-                                        <span>Rất tệ</span>
-                                    </div>
-                                </div>
-                            )}
-                        </div>
-                        <div className="rate-item">
-                            <div className="rate-item-cate">Phục vụ</div>
-                            <Rate
-                                style={{ color: "red", fontSize: 32 }}
-                                onChange={(value) => handleChange("rateService", value)}
-                                value={rate.rateService}
-                            />
-                            {rate.rateService ? (
-                                <div className="rate-type">
-                                    <div className="rate-type-text">
-                                        <span>{desc[rate.rateService - 1]}</span>
-                                    </div>
-                                </div>
-                            ) : (
-                                <div className="rate-type">
-                                    <div className="rate-type-text">
-                                        <span>Rất tệ</span>
-                                    </div>
-                                </div>
-                            )}
-                        </div>
-                        <div className="rate-item">
-                            <div className="rate-item-cate">Giá cả</div>
-                            <Rate
-                                style={{ color: "red", fontSize: 32 }}
-                                onChange={(value) => handleChange("ratePrice", value)}
-                                value={rate.ratePrice}
-                            />
-                            {rate.ratePrice ? (
-                                <div className="rate-type">
-                                    <div className="rate-type-text">
-                                        <span>{desc[rate.ratePrice - 1]}</span>
-                                    </div>
-                                </div>
-                            ) : (
-                                <div className="rate-type">
-                                    <div className="rate-type-text">
-                                        <span>Rất tệ</span>
-                                    </div>
-                                </div>
-                            )}
-                        </div>
-
-
-                    </div>
-                    <div className="review_left">
-                        <h3>Địa diểm</h3>
-                        <div className="review-select-place" >
-                            <button onClick={handleOpen}>submit</button>
-
-                            <span>
-                                <EnvironmentOutlined /> Nhấn vào đây để chọn địa điểm
-                            </span>
-                        </div>
-                    </div>
-                </div>
-                <div className="review-input" style={{width:"50%"}}>
-                    <h3 className="title">Đánh giá của bạn</h3>
-                    <Input.TextArea
-                        value={content}
-                        onChange={(e) => setContent(e.target.value)}
-                        placeholder="Nhập nội dung đánh giá"
-                        autoSize={{ minRows: 4, maxRows: 6 }}
-                    />
-                </div>
-                <div className="review-input">
-                    <Button
-                        onClick={() => {
-                            if (fileRef.current) {
-                                fileRef.current.click();
-                            }
-                        }}
-                        icon={<CameraOutlined />}
-                    >
-                        Thêm ảnh
-                    </Button>
-                    <input
-                        ref={fileRef}
-                        type="file"
-                        multiple
-                        accept="image/*"
-                        hidden
-                        onChange={handleChangeImages}
-                    />
-
-                    <div className="show_images">
-                        {images.map((img, index) => (
-                            <div key={index} className="media-show">
-                                {img.camera ? (
-                                    imageShow(img.camera)
-                                ) : img.url ? (
-                                    <>
-                                        {img.url.match(/video/i)
-                                            ? videoShow(img.url)
-                                            : imageShow(img.url)}
-                                    </>
-                                ) : (
-                                    <>
-                                        {img.type.match(/video/i)
-                                            ? videoShow(URL.createObjectURL(img))
-                                            : imageShow(URL.createObjectURL(img))}
-                                    </>
-                                )}
-                                <div className="btn">
-                                    <Button
-                                        icon={<CloseOutlined />}
-                                        onClick={() => deleteImages(index)}
-                                        type="primary"
-                                        danger
-                                    />
-                                </div>
-                            </div>
-                        ))}
-                    </div>
-                </div>
-                <div>
-                    <Button style={{backgroundColor:"red"}}
-                        onClick={handleSubmit}
-                        size="large"
-                        type="primary"
-                        shape="round"
-                    >
-                        Gửi đánh giá của bạn
-                    </Button>
-                </div>
-            </div>
+  return (
+    <div>
+      <Navbar />
+      <div className="Review_Container" style={{ witdt: "50%" }}>
+        <div className="header">
+          <h1>Viết Review</h1>
         </div>
-    );
+        <PostModal open={open} onClose={handleClose} />
+
+        <div className="Review">
+          <div className="review-input">
+            <h3>Xếp hạng của bạn</h3>
+            <div className="rate-item">
+              <div className="rate-item-cate">Xếp hạng</div>
+              <Rate
+                style={{ color: "red", fontSize: 32 }}
+                onChange={(value) => handleChange("rate", value)}
+                value={rate.rate}
+              />
+              {rate.rate ? (
+                <div className="rate-type">
+                  <div className="rate-type-text">
+                    <span>{desc[rate.rate - 1]}</span>
+                  </div>
+                </div>
+              ) : (
+                <div className="rate-type">
+                  <div className="rate-type-text">
+                    <span>Rất tệ</span>
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+          <div className="review_left">
+            <h3>Địa diểm</h3>
+            <div className="review-select-place" onClick={handleOpen}>
+              <span>
+                <EnvironmentOutlined /> Nhấn vào đây để chọn địa điểm
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="review-input" style={{ width: "50%" }}>
+          <h3 className="title">Đánh giá của bạn</h3>
+          <Input.TextArea
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+            placeholder="Nhập nội dung đánh giá"
+            autoSize={{ minRows: 4, maxRows: 6 }}
+          />
+        </div>
+        <div className="review-input">
+          <Button
+            onClick={() => {
+              if (fileRef.current) {
+                fileRef.current.click();
+              }
+            }}
+            icon={<CameraOutlined />}
+          >
+            Thêm ảnh
+          </Button>
+          <input
+            ref={fileRef}
+            type="file"
+            name="photo"
+            multiple
+            accept="image/*"
+            hidden
+            // onChange={handleUploadImage}
+            onChange={(e) => handleChangeFileImage(e)}
+          />
+          <div className="show_images">
+            {image && (
+              <img
+                style={{ width: "200px", display: "flex" }}
+                src={image}
+                alt=""
+              />
+            )}
+
+            <div className="media-show">
+              <div className="btn">
+                <Button
+                  icon={<CloseOutlined />}
+                  onClick={() => deleteImages()}
+                  type="primary"
+                  danger
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <Button
+            style={{ backgroundColor: "red" }}
+            onClick={handleSubmit}
+            size="large"
+            type="primary"
+            shape="round"
+          >
+            Gửi đánh giá của bạn
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Review;
--- fe/src/pages/review/modal/index.jsx
+++ fe/src/pages/review/modal/index.jsx
@@ -1,69 +1,66 @@
 import "./index.scss";
 import React, { useState } from "react";
-import { makeStyles } from '@material-ui/core/styles';
-import { Button, Modal, TextField } from '@material-ui/core';
+import { makeStyles } from "@material-ui/core/styles";
+import { Button, Modal, TextField } from "@material-ui/core";
 // import { Button, Modal, TextField } from '@mui/material';
+import logo1 from "../../../components/navbar/images/acount.jpeg";
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: 600,
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+  },
+}));
 
+const PostModal = ({ open, onClose }) => {
+  const classes = useStyles();
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
 
-const useStyles =makeStyles ((theme) => ({
-    paper: {
-      position: 'absolute',
-      width: 400,
-      backgroundColor: theme.palette.background.paper,
-      border: '2px solid #000',
-      boxShadow: theme.shadows[5],
-      padding: theme.spacing(2, 4, 3),
-      top: '50%',
-      left: '50%',
-      transform: 'translate(-50%, -50%)',
-    },
-  }));
-  
-  const PostModal = ({ open, onClose }) => {
-    const classes = useStyles();
-    const [title, setTitle] = useState('');
-    const [content, setContent] = useState('');
-  
-    const handleTitleChange = (e) => setTitle(e.target.value);
-    const handleContentChange = (e) => setContent(e.target.value);
-  
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      // Handle form submission here
-      onClose();
-    };
-  
-    return (
-      <Modal
-        open={open}
-        onClose={onClose}
-      >
-        <div className={classes.paper}>
-          <h2>Create a new post</h2>
-          <form onSubmit={handleSubmit}>
-            <TextField
-              label="Title"
-              value={title}
-              onChange={handleTitleChange}
-              fullWidth
-              margin="normal"
-            />
-            <TextField
-              label="Content"
-              value={content}
-              onChange={handleContentChange}
-              fullWidth
-              margin="normal"
-              multiline
-              rows={4}
-            />
-            <Button type="submit" color="primary" variant="contained">
-              Submit
-            </Button>
-          </form>
-        </div>
-      </Modal>
-    );
+  const handleTitleChange = (e) => setTitle(e.target.value);
+  const handleContentChange = (e) => setContent(e.target.value);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission here
+    onClose();
   };
-  
-  export default PostModal;
+
+  return (
+    <Modal open={open} onClose={onClose}>
+      <div className={classes.paper}>
+        <div className="modale-header-search">
+          <h2>Chọn địa điểm đánh giá</h2>
+          {/* <i class="bx  bx-x"></i> */}
+        </div>
+        <form onSubmit={handleSubmit}>
+          <div className="modale-body-search">
+            <input type="text" placeholder="Tìm kiếm địa điểm" />
+            <i className="bx bx-search-alt-2"></i>
+          </div>
+          <div className="modale-conten-search">
+            <div className="images">
+              <img className="image" src={logo1} alt="" />
+            </div>
+            <div className="info">
+              <div className="name">
+                <span>Chùa nam sơn</span>
+              </div>
+              <div className="address">
+                <span>đà nẵng quảng nam</span>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </Modal>
+  );
+};
+
+export default PostModal;
