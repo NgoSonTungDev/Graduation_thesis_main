fb08aa5116d87fce4e65ef88f72de7557e3cfae8
ngotungdev <tungns@danateq.vn>
2023-03-13 19:53:18 +0700

    page statistic

--- fe/src/App.js
+++ fe/src/App.js
@@ -7,6 +7,7 @@ import ChatBox from "./components/chat_box";
 import ChatBot from "./components/chat_gpt_fake";
 import AdminHome from "./pages/admin/home";
 import AdminMessage from "./pages/admin/message";
+import Statistic from "./pages/admin/statistic";
 import Register from "./pages/auth/register";
 import Home from "./pages/home";
 import NotFound from "./pages/not_found";
@@ -27,6 +28,7 @@ const App = () => {
           <Route path="/review" element={<Review />} />
           <Route path="/admin/home" element={<AdminHome />} />
           <Route path="/admin/message" element={<AdminMessage />} />
+          <Route path="/admin/statistic" element={<Statistic />} />
 
           <Route path="*" element={<NotFound />} />
         </Routes>
