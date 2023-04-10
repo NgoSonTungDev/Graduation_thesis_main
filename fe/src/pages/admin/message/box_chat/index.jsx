5639edac670107740c6cec4c64bfa54b643b4271
ngotungdev <tungns@danateq.vn>
2023-04-10 19:38:28 +0700

    fix message

--- fe/src/pages/admin/message/box_chat/index.jsx
+++ fe/src/pages/admin/message/box_chat/index.jsx
@@ -8,6 +8,7 @@ import { addMessage } from "../../../../redux/chat_box/chatBoxSlice";
 import ws from "../../../../socket";
 import { useDispatch, useSelector } from "react-redux";
 import { listChatBox } from "../../../../redux/selectors";
+import { toastify } from "../../../../utils/common";
 
 const BoxChat = ({ data, openDetail, callBackFunction }) => {
   const [message, setMessage] = useState("");
@@ -16,6 +17,9 @@ const BoxChat = ({ data, openDetail, callBackFunction }) => {
 
   const handleOnClickEnter = (e) => {
     if (e.key === "Enter") {
+      if(message===""){
+        return toastify("info","Vui lòng nhập tin nhắn !")
+      }
       sendMessage();
       callBackFunction();
     }
