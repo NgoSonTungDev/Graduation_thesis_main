c7badb672de2422566eca51a6535082b90118727
ngotungdev <tungns@danateq.vn>
2023-04-19 11:47:45 +0700

    fix be add timeslan

--- be/models/comment.ts
+++ be/models/comment.ts
@@ -2,22 +2,27 @@ import { IComment } from "./../types/comment";
 import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
-const commentSchema = new mongoose.Schema<IComment>({
-  userId: { type: Schema.Types.ObjectId, ref: "User" },
-  content: {
-    type: String,
-    required: true,
+const commentSchema = new mongoose.Schema<IComment>(
+  {
+    userId: { type: Schema.Types.ObjectId, ref: "User" },
+    content: {
+      type: String,
+      required: true,
+    },
+    dateTime: {
+      type: Number,
+      required: true,
+    },
+    like: [{ type: String, required: false, default: [] }],
+    postId: {
+      type: String,
+      required: true,
+    },
   },
-  dateTime: {
-    type: Number,
-    required: true,
-  },
-  like: [{ type: String, required: false, default: [] }],
-  postId: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Comments = mongoose.model("Comment", commentSchema);
 
